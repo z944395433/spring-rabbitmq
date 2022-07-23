3a9bdc0fee2a45676339612e050c58ebc1b5d6c1
unknown <z944395433>
2022-07-23 22:49:07 +0800

    init

---
+++ src/test/java/com/yiys/rabbitmqspring/RabbitmqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiys.rabbitmqspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
